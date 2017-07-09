e559d198ea508d27e6f223b6637002716d1d186e
Jason Kumpf <jason@kumpf.io>
2017-07-09 09:58:35 -0400

    Fixed issue with docker and standalone needing to know different paths for saved.sqlite file.

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN cd /src && go get -d -v ./... && go build -o goapp
 
 # final stage
 FROM alpine
+ENV SQLITE_PATH /data/saved.sqlite
 VOLUME /data
 WORKDIR /app
 COPY --from=build-env /src/goapp /app/
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import "os"
+
 func main() {
+	dbpath := os.Getenv("SQLITE_PATH")
+	if dbpath == "" {
+		dbpath = "saved.sqlite"
+	}
 	a := App{}
-	a.Initialize("saved.sqlite")
+	a.Initialize(dbpath)
 	a.Run(":5555")
 }
